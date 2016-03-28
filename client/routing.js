a2288b2295bbd466f4ae1f8cbe529fb022cb6d36
Marcelo Borges Pereira <borgespereira.marcelo@gmail.com>
2016-03-28 00:24:51 -0300

    add support for sass

--- client/routing.js
+++ client/routing.js
@@ -6,12 +6,3 @@ Router.map(function() {
   this.route('home', {path: '/'});
   this.route('stepper', {path: '/components/stepper'});
 });
-
-//Router.route('/components/stepper', function () {
- // this.render('Header');
- // this.render('Stepper');
-//});
-
-
-  // render the PostFooter template into the yield region named "footer" 
-  // {{> yield "footer"}}
