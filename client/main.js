6e4860fd1366f97fb38caad26cc23c84ca1fbe88
Marcelo Borges Pereira <borgespereira.marcelo@gmail.com>
2016-06-19 11:31:30 -0300

    Load form components on every page change

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 import '../imports/ui/signup/students.js';
 import '../imports/ui/signup/companies.js';
 import '../imports/ui/signup/stepper.js';
-import '../imports/ui/signup/masks.js';
+import '../imports/ui/signup/form-components.js';
 
 
