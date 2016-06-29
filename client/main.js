a7c4dca68bdb0c0dc4cb074e4eebe521764da4c4
Caike Motta <caike07@gmail.com>
2016-06-28 21:18:55 -0300

    Partial update, started theme application

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import '../imports/ui/signup/students.js';
 import '../imports/ui/signup/companies.js';
 import '../imports/ui/signup/stepper.js';
 import '../imports/ui/signup/masks.js';
+import '../imports/ui/components/js/core/source/App.min.js';
 
 stepNumber=0;
 
