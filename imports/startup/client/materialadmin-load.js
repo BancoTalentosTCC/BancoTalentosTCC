e2b1900de055f3615498772f8bac708317689e19
Caike Motta <caike07@gmail.com>
2016-09-23 20:19:38 -0300

    Fixed toggle button bug

--- imports/startup/client/materialadmin-load.js
+++ imports/startup/client/materialadmin-load.js
@@ -2,10 +2,26 @@ import {
   Template
 } from 'meteor/templating';
 
-Template.HomeLayout.onRendered(loadApp);
-Template.PanelLayout.onRendered(loadApp);
 
-function loadApp() {		
+Template.HomeLayout.rendered = function() {
+  this.autorun(_.bind(function() {
+    Deps.afterFlush(function() {
+      // only initialize when DOM is ready
+      import '/imports/ui/materialadmin/core/source/AppForm.js';
+    });
+  }, this));
+}
+
+Template.PanelLayout.rendered = function() {
+  this.autorun(_.bind(function() {
+    Deps.afterFlush(function() {
+      // only initialize when DOM is ready
+      loadApp();
+    });
+  }, this));
+}
+
+function loadApp() {
   import '/imports/ui/materialadmin/core/source/App.js';		
   import '/imports/ui/materialadmin/core/source/AppNavigation.js';		
   import '/imports/ui/materialadmin/core/source/AppOffcanvas.js';		
