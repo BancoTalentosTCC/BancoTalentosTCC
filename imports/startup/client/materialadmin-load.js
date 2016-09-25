cbfdde37a9676c19bbeae2d1e3d325ac960aaf42
Caike Motta <caike07@gmail.com>
2016-09-25 19:53:14 -0300

    Fixed floating labels on home screen, allow switch tabs on settings panel, installed active-routes to mark icon as active

--- imports/startup/client/materialadmin-load.js
+++ imports/startup/client/materialadmin-load.js
@@ -8,6 +8,7 @@ Template.HomeLayout.rendered = function() {
     Deps.afterFlush(function() {
       // only initialize when DOM is ready
       import '/imports/ui/materialadmin/core/source/AppForm.js';
+      fixFloatingLabels();
     });
   }, this));
 }
@@ -30,3 +31,25 @@ function loadApp() {
   import '/imports/ui/materialadmin/core/source/AppNavSearch.js';		
   import '/imports/ui/materialadmin/core/source/AppVendor.js';
 }
+
+function fixFloatingLabels() {
+  var o = this;
+  $('.floating-label .form-control').on('keyup change', function(e) {
+    var input = $(e.currentTarget);
+    if ($.trim(input.val()) !== '') {
+      input.addClass('dirty').removeClass('static');
+    } else {
+      input.removeClass('dirty').removeClass('static');
+    }
+  });
+  $('.floating-label .form-control').each(function() {
+    var input = $(this);
+    if ($.trim(input.val()) !== '') {
+      input.addClass('static').addClass('dirty');
+    }
+  });
+  $('.floating-label .form-control').each(function() {
+    $(this).after('<div class="form-control-line"></div>');
+  });
+}
+
--- imports/ui/pages/panel/settings.js
+++ imports/ui/pages/panel/settings.js
@@ -3,10 +3,16 @@ import {
 } from 'meteor/templating';
 
 Template.settings.rendered = function(){
-
+  //summernote 
   if (! $('#aboutme').data('summernote')) {
       $('#aboutme').summernote({
         height: $('#aboutme').height() + 150
       });
   };
+
+  //tabs
+  $('.nav-tabs a').click(function (e) { 
+    // e.preventDefault(); 
+    $(this).tab('show'); 
+  });
 }
