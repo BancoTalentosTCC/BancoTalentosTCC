b71b696ac7d4f8e79e81a6501d7083e80b5d02ba
Caike Motta <caike07@gmail.com>
2016-10-07 15:34:46 -0300

    Added remember me function, hide categories on mobile

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,7 +1,9 @@
 import {
   Template
 } from 'meteor/templating';
-
+import { 
+  Session 
+} from 'meteor/session';
 import '/client/html/pages/home.html';
 import '/imports/ui/components/form-field.js';
 
@@ -10,6 +12,10 @@ Template.home.onRendered(function() {
       interval: 6000,
       cycle: true
     });
+  
+  // setting a saved user if true
+  $("#email").val(Session.get("email"));
+  $("#password").val(Session.get("password"));
 });
 
 Template.home.events({
@@ -18,6 +24,12 @@ Template.home.events({
     //REMOVE ERRORS
     $('.warning').removeClass('warning');
 
+    console.log(event);
+    if ( $('#rememberme:checkbox:checked').length > 0) {
+      Session.setPersistent("email", event.target["email"].value);
+      Session.setPersistent("password", event.target["password"].value);
+    }
+    
     Meteor.loginWithPassword($('#email').val(), $("#password").val(), function(error) {
       if (error) {
         Meteor.call('displayErrors', error);
