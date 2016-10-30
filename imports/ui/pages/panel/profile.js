55dbbea925043a625ecd98145b91030837cceba4
Caike Motta <caike07@gmail.com>
2016-10-30 11:58:33 -0200

    Student profile update

--- imports/ui/pages/panel/profile.js
+++ imports/ui/pages/panel/profile.js
@@ -5,5 +5,29 @@ Template.profile.helpers({
     var id = FlowRouter.getParam('id');
     if (id) return Meteor.users.find({_id: id}).fetch()[0];
     return Meteor.user();
+  },
+  languageLevel: function(nivel) {
+    switch(nivel) {
+      case "basico":
+        return "30%";
+      case "intermed":
+        return "50%";
+      case "avanc":
+        return "80%";
+      case "flue":
+        return "100%";
+    }
+  },
+  calculateYear: function(duration) {
+    if (duration < 2) {
+      return duration + " ano";
+    }
+    else return duration + " anos";
   }
+});
+
+Template.profile.onRendered(function(){
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 });
